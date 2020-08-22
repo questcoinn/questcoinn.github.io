9dcf623c14f314c198e059e046c0acea47ef4f40
questcoin <iopell2007@gmail.com>
2020-08-22 17:04:42 +0900

    Add userstyles

--- scripts/test.js
+++ scripts/test.js
@@ -1,25 +1,28 @@
 !function() {
 
+const TOTAL_FLAKES = 20;
+const INTERVAL = 500;
+const BASE_DURATION = 1;
+const RANDOM_DURATION = 5;
+
 const bench = document.getElementById("test-bench");
 const make = (text) => {
     const ret = document.createElement("div");
     ret.style.left = `${100 * Math.random()}%`;
-    ret.style.animationDuration = `${10 + 40 * Math.random()}s`;
+    ret.style.animationDuration = `${BASE_DURATION + RANDOM_DURATION * Math.random()}s`;
     ret.innerText = text;
     return ret;
 }
-const totalFlakes = 50;
-const interval = 2000;
-let start;
 
+let start;
 const snow = (timestamp) => {
     if(!start) start = timestamp;
     const progress = timestamp - start;
 
-    bench.append(make("❄️"));
+    bench.append(make("💧"));
 
-    if(progress < totalFlakes * interval) {
-        setTimeout(() => requestAnimationFrame(snow), interval);
+    if(progress < TOTAL_FLAKES * INTERVAL) {
+        setTimeout(() => requestAnimationFrame(snow), INTERVAL);
     }
 }
 
